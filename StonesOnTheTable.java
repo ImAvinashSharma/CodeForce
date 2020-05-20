f5f940c6fccad56fd0a5d45b605fbf7e89f9ddde
Avinash Sharma <58262578+avi9360@users.noreply.github.com>
2020-05-20 14:19:26 +0530

    Update StonesOnTheTable.java
    236A - Stones On The Table

--- StonesOnTheTable.java
+++ StonesOnTheTable.java
@@ -2,11 +2,22 @@ package avinashCode;
 import java.util.Scanner;
 public class StonesOnTheTable {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int count=0;
 		String str = sc.next();
-		System.out.println(str);
+		char ch[] = str.toCharArray();
+		for(int i=0;i<str.length();i++) {
+			try {
+			if(ch[i]==ch[i+1]) 
+				count++;
+		}
+			catch(Exception e) {
+				break;
+			}
+		}
+ 		System.out.println(count);
 		sc.close();
 	}
 
