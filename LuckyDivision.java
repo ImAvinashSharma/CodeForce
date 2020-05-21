5e1f4f5120d8e728d89dfd86b44767121fc760cc
Avinash Sharma <58262578+avi9360@users.noreply.github.com>
2020-05-21 11:46:46 +0530

    Update LuckyDivision.java

--- LuckyDivision.java
+++ LuckyDivision.java
@@ -1,10 +1,20 @@
-package day2;
-
+import java.util.Scanner;
 public class LuckyDivision {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		boolean f = false;
+		int arr[] = {4,7,47,74,44,444,447,474,477,777,774,744};
+		for(int i = 0;i<12;i++) {
+			if(num%arr[i]==0) {
+				f = true;
+			}
+		}
+		if(f)
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+		sc.close();
 	}
-
 }
